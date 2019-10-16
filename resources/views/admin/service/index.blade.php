7149d446f1796dcc722bc9d65c61a81ffd8e31a6
Emilabbas679 <emilabbas679@gmail.com>
2019-10-16 12:47:25 +0400

    Subscription and services

---
+++ resources/views/admin/service/index.blade.php
@@ -0,0 +1,66 @@
+@extends("admin.layout")
+@section('title', 'Subscriptions')
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>User</th>
+                        <th>Package id</th>
+                        <th>Payment status</th>
+                        <th>Status</th>
+                        <th>Account Number</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                        <th>Operations</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <tr>
+                        <th>#</th>
+                        <th>User</th>
+                        <th>Package id</th>
+                        <th>Payment status</th>
+                        <th>Status</th>
+                        <th>Account Number</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                        <th>Operations</th>
+                    </tr>
+                    </tfoot>
+                    <tbody>
+                    @foreach ($subscriptions as $sub)
+                        <tr>
+                            <td>{{ $sub->id }}</td>
+                            <td>{{ $sub->user->name }}</td>
+                            <td>{{ $sub->package->name }}</td>
+                            <td>{{$sub->payment_status}}</td>
+                            <td>{{$sub->status}}</td>
+                            <td>{{$sub->account_number}}</td>
+                            <td>{{ $sub->created_at }}</td>
+                            <td>{{ $sub->updated_at }}</td>
+
+                            <td>
+
+                                <form id="delete-form" action="{{ route('user.destroy', $sub->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+{{--                                    <a href="{{route('subscription.show',$sub->id)}}" class="btn btn-primary btn-circle btn-sm">--}}
+{{--                                        <i class="far fa-eye"></i>--}}
+{{--                                    </a>--}}
+                                    <button class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
