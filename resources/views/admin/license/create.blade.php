2c8e3568d39e8104e7856c81466e996a391c4473
Emilabbas679 <emilabbas679@gmail.com>
2019-10-18 11:47:24 +0400

    license updated

--- resources/views/admin/license/create.blade.php
+++ resources/views/admin/license/create.blade.php
@@ -24,37 +24,10 @@
                             @enderror
                         </div>
 
-                        <div class="form-group">
-                            <select name="user_id" id="" class="form-control">
-                                <option value="">Select User</option>
-                                @foreach($users as $user)
-                                    <option value="{{$user->id}}">{{$user->id}}- {{$user->name}}</option>
-                                    @endforeach
-                            </select>
-                            @error('user_id')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <select name="status"  class="form-control">
-                                <option value="">Select Status</option>
-                                <option value="1">Active</option>
-                                <option value="0">Deactive</option>
-
-                            </select>
-                            @error('user_id')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
 
                         <div class="form-group">
                             <div class="form-check">
-                                <input type="checkbox" value="1" name="is_active" class="form-check-input" id="exampleCheck1" {{ old('is_active') ? 'checked' : '' }}>
+                                <input type="checkbox" checked value="1" name="is_active" class="form-check-input" id="exampleCheck1" {{ old('is_active') ? 'checked' : '' }}>
                                 <label class="form-check-label" for="exampleCheck1">{{ __('Active') }}</label>
                             </div>
                         </div>
